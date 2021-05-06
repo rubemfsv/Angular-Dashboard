f3d7479c7f5eae1af3da403ed99f8d831f2559d9
Rubem <rfsv@ic.ufal.br>
2021-05-05 21:01:30 -0300

    chore: implements data service

--- src/app/dashboard/datas.service.ts
+++ src/app/dashboard/datas.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DatasService {
+  readonly datas = [
+    ['Janeiro', 33],
+    ['Fevereiro', 68],
+    ['Março', 49],
+    ['Abril', 15],
+    ['Maio', 80],
+    ['Junho', 27],
+  ];
 
-  constructor() { }
+  constructor() {}
+
+  /**
+   * Returns an observable containing tha datas to be
+   * showed in the graph
+   *
+   * @return Observable<any>
+   */
+  obtainsData(): Observable<any> {
+    return new Observable((observable) => {
+      observable.next(this.datas);
+      observable.complete();
+    });
+  }
 }
